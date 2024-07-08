683863900f57ac92a91f396ab36e2f23094cc1f0
Sankararaman Vedagiri <shnkar007@hotmail.com>
2024-07-08 20:59:13 +0530

    Update GitTest.java in the second line

--- GitTest.java
+++ GitTest.java
@@ -1,6 +1,6 @@
 public class GitTest{
 	public static void main(String... args){
 		System.out.println("First line");
-		System.out.println("Second line");
+		System.out.println("Second line changed");
 	}
 }
